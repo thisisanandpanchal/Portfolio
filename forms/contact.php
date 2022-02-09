fbdc73edbd877179d133cf49c6d15e3b9099f204
Anand <panchalanand26031@gmail.com>
2022-02-09 13:58:47 +0530

    portfolio

---
+++ forms/contact.php
@@ -0,0 +1,8 @@
+<?php 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    
+}
