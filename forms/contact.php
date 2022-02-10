b27c9bb199ba0f05659a255c6ab9e5dad0d07fbe
Anand <panchalanand26031@gmail.com>
2022-02-10 13:01:35 +0530

    initial commit

--- forms/contact.php
+++
@@ -1,8 +0,0 @@
-<?php 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    
-}
